be7370e19dd213492cb9d121062db752869818eb
Alexander Ivanov <33380175+xanderhub@users.noreply.github.com>
2023-10-31 17:17:39 +0200

    4th test refactor

--- src/word-wrap.ts
+++ src/word-wrap.ts
@@ -9,18 +9,18 @@ export function wrapText(text: string, width: number): string {
         let line: string = text.substring(index, index + width);
         if (line.lastIndexOf(' ') == -1) {
             result = result + line + '\n';
-            index += width;
-            if (text.charAt(index) === ' ') {
-                index++;
-            }
+            index = moveIndex(text, index, width);
         } else {
             result = result + line.substring(0, line.lastIndexOf(' ')) + '\n';
-            index += line.lastIndexOf(' ');
-            if (text.charAt(index) === ' ') {
-                 index++;
-            }
+            index = moveIndex(text, index, line.lastIndexOf(' '));
         }
     }
 
     return result + text.substring(index).trim();
 }
+
+function moveIndex(text: string, index: number, offset: number): number {
+    index += offset;
+    return text.charAt(index) === ' ' ? ++index : index
+}
+
